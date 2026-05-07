9e34d96ea3f71ab74098810225386f7086f742de
Tony Wang <yilongwang05@gmail.com>
2026-05-07 16:56:44 -0700

    feat: remove unused location permissions and logic

--- lib/core/services/permission_service.dart
+++ lib/core/services/permission_service.dart
@@ -3,7 +3,6 @@ import 'package:permission_handler/permission_handler.dart';
 class PermissionService {
   Future<void> requestInitialPermissions() async {
     await [
-      Permission.location,
       Permission.notification,
     ].request();
   }
